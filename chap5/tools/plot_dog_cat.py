8483926a79726875552b7696e19d78df400a9ecf
natsutan <YRW03704@nifty.com>
2018-12-27 14:12:41 +0900

    ファイル整理

---
+++ chap5/tools/plot_dog_cat.py
@@ -0,0 +1,28 @@
+import pickle
+import os
+import matplotlib.pyplot as plt
+
+save_dir = '../save'
+history_save = os.path.join(save_dir, 'history_tpu.pickle')
+
+history = pickle.load(open(history_save, 'rb'))
+
+acc = history['acc']
+val_acc = history['val_acc']
+loss = history['loss']
+val_loss = history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and Validation accuracy(TPU)')
+plt.legend()
+plt.savefig(os.path.join(save_dir, 'plot1_tpu.png'))
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation acc')
+plt.title('Training and Validation loss(TPU)')
+plt.legend()
+
+plt.savefig(os.path.join(save_dir,'plot2_tpu.png'))
