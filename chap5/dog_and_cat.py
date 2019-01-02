06e9bcee45c814f0af77c09f056f2837d2ca2790
natsutan <km1km1km1>
2019-01-02 19:54:50 +0900

    work

--- chap5/dog_and_cat.py
+++ chap5/dog_and_cat.py
@@ -28,7 +28,7 @@ train_generator = train_datagen.flow_from_directory(
     batch_size=20,
     class_mode='binary'
 )
-
+    
 validation_generator = test_datagen.flow_from_directory(
     validation_dir,
     target_size=(150, 150),
